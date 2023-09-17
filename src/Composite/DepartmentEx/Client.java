ed299cdf960e4506c3f7d876a14e316be74c9e61
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2023-09-17 22:07:07 +0530

    Added Facade Pattern

---
+++ src/Composite/DepartmentEx/Client.java
@@ -0,0 +1,20 @@
+package Composite.DepartmentEx;
+
+public class Client {
+    public static void main(String[] args) {
+        //SpringApplication.run(CompositeApplication.class, args);
+        Department salesDepartment = new SalesDepartment(
+                1, "Sales department");
+        Department financialDepartment = new FinancialDepartment(
+                2, "Financial department");
+
+        HeadDepartment headDepartment = new HeadDepartment(
+                3, "Head department");
+
+        headDepartment.addDepartment(salesDepartment);
+        headDepartment.addDepartment(financialDepartment);
+
+        headDepartment.printDepartmentName();
+    }
+
+}
