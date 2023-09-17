45a21d4644be6a275f8fef5d6fe81a5b207f5f88
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2023-09-17 22:11:39 +0530

    Added Adapter Pattern

---
+++ src/Adapter/DuckEx/Duck.java
@@ -0,0 +1,6 @@
+package Adapter.DuckEx;
+
+public interface Duck {
+    public void quack();
+    public void fly();
+}
---
+++ src/Adapter/DuckEx/Turkey.java
@@ -0,0 +1,6 @@
+package Adapter.DuckEx;
+
+public interface Turkey {
+    public void gobble();
+    public void fly();
+}
